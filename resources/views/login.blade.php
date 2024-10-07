a929b5b7dbd0986ae9b2e5b20a46b1f6ee5eeae2
Ivan Axel <cupido.iacp@gmail.com>
2024-10-06 21:49:33 -0600

    nuevo login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,46 +17,31 @@
     <main class="container align-center p-5">
         <form method="POST" action="{{ route('inicia-sesion') }}">
             @csrf
-            <div class="container">
-
-                <!-- Outer Row -->
-                <div class="row justify-content-center">
-
-                    <div class="col-xl-10 col-lg-12 col-md-9">
-
-                        <div class="card o-hidden border-0 shadow-lg my-5">
-                            <div class="card-body p-0">
-                                <!-- Nested Row within Card Body -->
-                                <div class="row">
-                                    <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                                        <img src="{{ asset('logo.webp') }}">
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="p-5">
-                                            <div class="text-center">
-                                                <h1 class="h4 text-gray-900 mb-4">Bienvenido</h1>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="email" class="form-control form-control-user"
-                                                    id="emailinput" placeholder="Ingresa tu correo Electronico"
-                                                    name="email" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password" class="form-control form-control-user"
-                                                    id="passwordInput" placeholder="Contraseña" name="password"
-                                                    required>   
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="custom-control custom-checkbox small">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck" name="remember">
-                                                    <label class="custom-control-label" for="customCheck">Mantener sesión iniciada</label>
-                                                </div>
-                                            </div>
-                                            <button type="submit" class="btn btn-primary">Acceder</button>
-                                            <hr>
-                                            <div class="text-center">
-                                                <a class="small" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
-                                            </div>
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-xl-10">
+                        <div class="card" style="border-radius: 1rem;">
+                            <div class="row g-0">
+                                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                                </div>
+                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                    <div class="card-body p-4 p-lg-5 text-black">
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                            <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                            <span class="h1 fw-bold mb-0">Logo</span>
+                                        </div>
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+                                        <div class="form-outline mb-4">
+                                            <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" required />
+                                            <label class="form-label" for="form2Example17">Email address</label>
+                                        </div>
+                                        <div class="form-outline mb-4">
+                                            <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" required />
+                                            <label class="form-label" for="form2Example27">Password</label>
+                                        </div>
+                                        <div class="pt-1 mb-4">
+                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
                                         </div>
                                         <a class="small text-muted" href="#!">Forgot password?</a>
                                         <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!" style="color: #393f81;">Register here</a></p>
